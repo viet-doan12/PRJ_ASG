24947b75cb493526c0eab942cc38116c6f7178f5
catlovefanta <minhtarn119@gmail.com>
2026-07-07 21:48:26 +0700

    test

--- src/java/com/flowershop/model/Flower.java
+++ src/java/com/flowershop/model/Flower.java
@@ -4,104 +4,121 @@
  */
 package com.flowershop.model;
 
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
 /**
  *
  * @author ADMIN
  */
 public class Flower {
-
-    private int flowerId;
+    private int flowerID;
     private String flowerName;
-    private double price;
-    private int quantity;
     private String description;
+    private BigDecimal price;
+    private int stockQuantity;
     private String image;
-    private int categoryId;
+    private int categoryID;
     private boolean status;
+    private Timestamp createdDate;
+    private Timestamp updatedDate;
 
     public Flower() {
     }
 
-    public Flower(int flowerId, String flowerName, double price,
-                  int quantity, String description, String image,
-                  int categoryId, boolean status) {
-        this.flowerId = flowerId;
+    public Flower(int flowerID, String flowerName, String description,
+            BigDecimal price, int stockQuantity, String image,
+            int categoryID, boolean status,
+            Timestamp createdDate, Timestamp updatedDate) {
+        this.flowerID = flowerID;
         this.flowerName = flowerName;
-        this.price = price;
-        this.quantity = quantity;
         this.description = description;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
         this.image = image;
-        this.categoryId = categoryId;
+        this.categoryID = categoryID;
         this.status = status;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
     }
 
-    public int getFlowerId() {
-        return flowerId;
+    public int getFlowerID() {
+        return flowerID;
+    }
+
+    public void setFlowerID(int flowerID) {
+        this.flowerID = flowerID;
     }
 
     public String getFlowerName() {
         return flowerName;
     }
 
-    public double getPrice() {
-        return price;
-    }
-
-    public int getQuantity() {
-        return quantity;
+    public void setFlowerName(String flowerName) {
+        this.flowerName = flowerName;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public String getImage() {
-        return image;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    public int getCategoryId() {
-        return categoryId;
+    public BigDecimal getPrice() {
+        return price;
     }
 
-    public boolean isStatus() {
-        return status;
-    }
-
-    public void setFlowerId(int flowerId) {
-        this.flowerId = flowerId;
-    }
-
-    public void setFlowerName(String flowerName) {
-        this.flowerName = flowerName;
-    }
-
-    public void setPrice(double price) {
+    public void setPrice(BigDecimal price) {
         this.price = price;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
+    public int getStockQuantity() {
+        return stockQuantity;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getImage() {
+        return image;
     }
 
     public void setImage(String image) {
         this.image = image;
     }
 
-    public void setCategoryId(int categoryId) {
-        this.categoryId = categoryId;
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public boolean isStatus() {
+        return status;
     }
 
     public void setStatus(boolean status) {
         this.status = status;
     }
 
-    @Override
-    public String toString() {
-        return "Flower{" + "flowerId=" + flowerId + ", flowerName=" + flowerName + ", price=" + price + ", quantity=" + quantity + ", description=" + description + ", image=" + image + ", categoryId=" + categoryId + ", status=" + status + '}';
+    public Timestamp getCreatedDate() {
+        return createdDate;
     }
-    
+
+    public void setCreatedDate(Timestamp createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Timestamp getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Timestamp updatedDate) {
+        this.updatedDate = updatedDate;
+    }
 }
